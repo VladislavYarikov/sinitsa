26b4ae06d7324b19346a2844be3ed7fdf081224a
Vladislav <redytdel@gmail.com>
2025-05-06 19:47:34 +0300

    test10

--- api/bot.js
+++ api/bot.js
@@ -1,4 +1,5 @@
 import TelegramBot from 'node-telegram-bot-api';
+import { Telegraf } from 'telegraf';
 import { InferenceClient } from "@huggingface/inference";
 import 'dotenv/config';
 
@@ -21,32 +22,38 @@ const generateAnswer = async (message) => {
 }
 
 const client = new InferenceClient(apiKey); //client for AI messages
-const bot = new TelegramBot(API_KEY_BOT, { polling: false });
+//const bot = new TelegramBot(API_KEY_BOT, { polling: false });
+const bot = new Telegraf(API_KEY_BOT);
 
 // bot.on("polling_error", err => console.log(err.data.error.message));
 
-bot.on("text", (msg) => {
-    const chatId = msg.chat.id;
-    //console.log(msg)
+// bot.on("text", (msg) => {
+//     const chatId = msg.chat.id;
+//     //console.log(msg)
     
-    bot.sendMessage(chatId, 'Подожди...')
-       .then(sentMessage => { console.log(sentMessage); generateAnswer(msg.text)
-       .then(response => {
-                bot.editMessageText(response, {
-                chat_id: chatId,
-                message_id: sentMessage.message_id
-            });
-        });
-    });
-});
+//     bot.sendMessage(chatId, 'Подожди...')
+//        .then(sentMessage => { console.log(sentMessage); generateAnswer(msg.text)
+//        .then(response => {
+//                 bot.editMessageText(response, {
+//                 chat_id: chatId,
+//                 message_id: sentMessage.message_id
+//             });
+//         });
+//     });
+// });
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.on('text', (ctx) => {
+    ctx.reply('Hi from Vercel!');
+  });
+bot.launch();
 
 export default async function handler(req, res) {
-    console.log(bot, API_KEY_BOT)
+    console.log(bot)
 
   if (req.method === 'POST') {
     try {
       // Process the update from the request body
-      bot.processUpdate(req.body);
+      await bot.handleUpdate(req.body, res);
       res.status(200).send('ok');
     } catch (error) {
       res.status(500).send('Error processing update');
