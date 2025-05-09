4054d719e908efed396709a03f8d793a93d8670d
Vladislav <redytdel@gmail.com>
2025-05-09 12:32:51 +0300

    mongo test 3

--- api/bot.js
+++ api/bot.js
@@ -4,16 +4,8 @@ import { connectDB } from './db.js';
 import { User } from '../models/User.js';
 import 'dotenv/config';
 
-
-console.log("hello")
 await connectDB();
-console.log("good")
-
-const user = await User.create({ name: 'Alice', email: 'alice@example.com' });
-console.log('New user:', user);
-
-const allUsers = await User.find();
-console.log('All users:', allUsers);
+await User.create({ name: 'Alice', email: 'alice@example.com' });
 
 const apiKey = process.env.HUGGINGFACE_TOKEN; // Hugging Face API-ключ
 const API_KEY_BOT = process.env.TELEGRAM_TOKEN; // Telegram bot API key
--- models/User.js
+++ models/User.js
@@ -3,6 +3,6 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
   name: String,
   email: String,
-});
+}, { collection: 'clients' });
 
 export const User = mongoose.model('User', userSchema);
